2c4ce0379c0e6eed43a57686aab55b44ad83fc76
emmanuel francis <D00228281@student.dkit.ie>
2023-02-20 15:45:59 +0000

    Added a contact form handler, thank you message and contacts page.

--- contact.php
+++ contact.php
@@ -1,9 +1,25 @@
 <?php include 'includes/header.php';?>
 <main class="container">
   <div class="starter-template text-center">
-    <h1>Contact us</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-  </div>
+  <form method="POST" name="contactform" action="contact-form-handler.php"> 
+<p>
+<label for='name'>Your Name:</label> <br>
+<input type="text" name="name">
+</p>
+<p>
+<label for='email'>Email Address:</label> <br>
+<input type="text" name="email"> <br>
+</p>
+<p>
+<label for='phone'>Phone Number:</label> <br>
+<input type="number" name="phone"> <br>
+</p>
+<p>
+<label for='message'>Message:</label> <br>
+<textarea name="message"></textarea>
+</p>
+<input type="submit" value="Submit"><br>
+</form>  </div>
 
 </main><!-- /.container -->
 <?php include 'includes/footer.php';?>
--- thank-you.php
+++ thank-you.php
@@ -1,8 +1,8 @@
 <?php include 'includes/header.php';?>
 <main class="container">
   <div class="starter-template text-center">
-    <h1>Contact us!</h1>
-    <p class="lead">Contact form will go here.</p>
+  <h1>Thank you!</h1>
+    Thank you for submitting the form. We will contact you soon!
   </div>
 
 </main><!-- /.container -->
